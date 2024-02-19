20ac30a4542dd36f44edf591c7a18eea9826a104
Cosmin Anghel <cosmin.anghel@metaplast.ro>
2024-02-19 15:35:22 +0200

    Add project files.

---
+++ lab1ex1-2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+* Scrietiun program care citind de la tastaura
+* cele trei dimensiuni ale unui paralelipiped dreptunghic,
+* va afisavolumul lui
+*/
+
+
+Console.WriteLine("Introduceti valoarea pentru latura a");
+int laturaA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru latura b");
+int laturaB = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Introduceti valoarea pentru latura c");
+int laturaC = int.Parse(Console.ReadLine());
+
+//Volum = laturaA * laturaB * laturaC
+int volumulParalelipipeduluiDreptunghic = laturaA * laturaB * laturaC;
+
+Console.WriteLine("Volumul paralelipepedului dreptunghic este:" + volumulParalelipipeduluiDreptunghic);
